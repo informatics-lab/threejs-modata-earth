e711c0e59623c6b12dce61e45b2e842465cd6ed4
Niall Robinson <niall.h.robinson@gmail.com>
2016-10-27 09:55:06 +0100

    added chart module

---
+++ src/js/globe-data/chart.js
@@ -0,0 +1,23 @@
+module.exports = function() {
+
+    var self = this;
+
+    function updateCO2(val){
+        var bar = document.getElementById("co2");
+        bar.innerHTML = val;
+        bar.height = val-285.0;
+    };
+
+    function updateTemp(val){
+        document.getElementById("temp").innerHTML = val;
+    };
+
+    return {
+        setCO2 : function(val) {
+            updateCO2(val);
+        },
+        setTemp : function(val) {
+            updateTemp(val)
+        }
+    }
+};
