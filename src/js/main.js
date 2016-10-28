d33319172c06b50fec5c399ff1463cc94a83ac4d
Thomas Powell <t.powell.meto@gmail.com>
2016-10-28 10:11:23 +0100

    work on loading

--- src/js/main.js
+++ src/js/main.js
@@ -1,75 +1,78 @@
 const MET_OFFICE = "Met Office";
 const INFORMATICS_LAB = "Informatics Lab";
 const APP_NAME = "HadCRUT4";
-
+const APP_DIV_ID = "content";
 const GLOBE_RADIUS = 1;
 
 
 var OrbitControls = require('three-orbit-controls')(THREE);
 var dat = require('dat-gui');
-// var pace = require('pace-progress');
+var pace = require('pace-progress');
 var rp = require('request-promise');
-
+// var THREE = require("three");
 
 var Globe = require('./globe');
 var GlobeData = require('./globe-data');
 
-// var THREE = require("three");
-
 var scene, camera, renderer, controls;
 
-// pace.start({
-//     ajax: false, // disabled
-//     document: false, // disabled
-//     eventLag: false, // disabled
-//     elements: {
-//         selectors: ['.ready']
-//     }
-// });
+
+
 init();
 
 // initialises scene
 function init() {
 
-    addAppTitle();
+    function pageLoading() {
+        pace.restart();
+        pace.on('done', function () {
+            document.getElementById(APP_DIV_ID).style.opacity = 1;
+        });
+    }
 
-    window.addEventListener('resize', onWindowResize, false);
+    pageLoading();
 
     var width = window.innerWidth;
+    var height = window.innerHeight;
 
+    //create threejs redenderer and attach it to dom
+    renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer.setSize(width, height);
+    renderer.setClearColor(0x000000);
+    document.getElementById('content').appendChild(renderer.domElement);
+
+
+    //create our scene
     scene = new THREE.Scene();
     scene.origin = new THREE.Vector3(0, 0, 0);
 
-    camera = new THREE.PerspectiveCamera(45, width / window.innerHeight, 0.01, 1000);
+
+    //create our camera
+    camera = new THREE.PerspectiveCamera(45, width / height, 0.01, 1000);
     camera.position.z = GLOBE_RADIUS * 3;
 
+
+    // add lights
     var ambientLight = new THREE.AmbientLight(0xffffff, 0.2); // soft white light
     scene.add(ambientLight);
-
     var directionalLight = new THREE.DirectionalLight(0xbbbbbb, 0.8);
     scene.add(directionalLight);
     directionalLight.position.copy(camera.position);
 
-    renderer = new THREE.WebGLRenderer({antialias: true});
-    renderer.setSize(width, window.innerHeight);
-    renderer.setClearColor(0x000000);
 
-    document.getElementById('content').appendChild(renderer.domElement);
-
+    //init controls
     controls = new OrbitControls(camera, renderer.domElement);
     controls.enablePan = false;
-    // controls.autoRotate = true;
     controls.minDistance = GLOBE_RADIUS * 2;
     controls.maxDistance = GLOBE_RADIUS * 3;
-    // controls.minPolarAngle = (Math.PI / 10) * 2.5; // radians
-    // controls.maxPolarAngle = (Math.PI / 10) * 6.5;
-
+    
+    //sync camera and directional light so we can see what we're doing!!
     controls.addEventListener('change', function (evt) {
         directionalLight.position.copy(camera.position);
     });
 
-    var globe = new Globe(scene, GLOBE_RADIUS);
 
+    //setup data to load in
     var hadcrut4_1year_mean_opts = {
         uri: document.location.origin + "/data/hadcrut4_1year_mean.json",
         json: true
@@ -79,8 +82,54 @@ function init() {
         json: true
     };
 
+
+    //load all data then we are ready to go!
     Promise.all([rp(hadcrut4_1year_mean_opts), rp(hadcrut4_annotations_opts)])
         .then(function (arr) {
+
+            /**
+             * Adds the title div to the app.
+             */
+            function addAppTitle() {
+
+                function addMOLogo(domElement) {
+                    var moLogo = document.createElement('img');
+                    moLogo.id = "moLogo";
+                    moLogo.src = "img/met-office-logo.svg";
+                    moLogo.alt = MET_OFFICE;
+                    domElement.appendChild(moLogo)
+                }
+
+                function addInfoLabLogo(domElement) {
+                    var infoLabLogo = document.createElement('h2');
+                    infoLabLogo.id = "infoLabLogo";
+                    infoLabLogo.innerHTML = INFORMATICS_LAB;
+                    domElement.appendChild(infoLabLogo);
+                }
+
+                function addAppName(domElement) {
+                    var appName = document.createElement('h1');
+                    appName.id = "appName";
+                    appName.innerHTML = APP_NAME;
+                    domElement.appendChild(appName);
+                }
+
+                var app = document.getElementById(APP_DIV_ID);
+
+                var titleDiv = document.createElement("div");
+                titleDiv.id = "appTitle";
+
+                addMOLogo(titleDiv);
+                addInfoLabLogo(titleDiv);
+                addAppName(titleDiv);
+
+                app.appendChild(titleDiv);
+
+            }
+
+            addAppTitle();
+
+            var globe = new Globe(scene, GLOBE_RADIUS);
             var data = new GlobeData.rawDataSphereMesh(scene, GLOBE_RADIUS * 1.02, arr[0], arr[1]);
 
             var gui = new dat.GUI();
@@ -104,61 +153,26 @@ function init() {
             guiCamFolder.add(camera.position, 'y', -5, 5).listen();
             guiCamFolder.add(camera.position, 'z', -5, 5).listen();
 
+            //begin animating stuff!
             animate();
         });
+
+    //resize threejs canvas if window size is changed during interaction.
+
+    function onWindowResize() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+
+    window.addEventListener('resize', onWindowResize, false);
 }
 
-// animates the scene
+// animation loop
 function animate(time) {
     scene.dispatchEvent({type: "animate", message: time});
     controls.update();
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
-}
-
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-}
-
-/**
- * Adds the title div to the app.
- */
-function addAppTitle() {
-
-    function addMOLogo(domElement) {
-        var moLogo = document.createElement('img');
-        moLogo.id = "moLogo";
-        moLogo.src = "img/met-office-logo.svg";
-        moLogo.alt = MET_OFFICE;
-        domElement.appendChild(moLogo)
-    }
-
-    function addInfoLabLogo(domElement) {
-        var infoLabLogo = document.createElement('h2');
-        infoLabLogo.id = "infoLabLogo";
-        infoLabLogo.innerHTML = INFORMATICS_LAB;
-        domElement.appendChild(infoLabLogo);
-    }
-
-    function addAppName(domElement) {
-        var appName = document.createElement('h1');
-        appName.id = "appName";
-        appName.innerHTML = APP_NAME;
-        domElement.appendChild(appName);
-    }
-
-    var app = document.getElementById("content");
-
-    var titleDiv = document.createElement("div");
-    titleDiv.id = "appTitle";
-
-    addMOLogo(titleDiv);
-    addInfoLabLogo(titleDiv);
-    addAppName(titleDiv);
-
-    app.appendChild(titleDiv);
-
 }
 
